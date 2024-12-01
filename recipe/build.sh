ac6a02acb99ad4961f7310d1596f1abf6cb13915
Taher Chegini <taher.chegini@gmail.com>
2024-12-01 17:24:46 -0500

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,9 +7,11 @@ set -o xtrace -o nounset -o pipefail -o errexit
 export CARGO_NET_GIT_FETCH_WITH_CLI=true
 
 # Bundle all downstream library licenses
+pushd src/catsmoothing
 cargo-bundle-licenses \
     --format yaml \
     --output ${SRC_DIR}/THIRDPARTY.yml
+popd
 
 maturin build --features python -v --jobs 1 --release  --strip --interpreter=$PYTHON
 $PYTHON -m pip install . -vv --no-deps --no-build-isolation || exit 1
