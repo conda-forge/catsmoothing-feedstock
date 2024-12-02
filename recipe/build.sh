e7110fedf85fe199100faa66cba941797067614b
Taher Chegini <cheginit@gmail.com>
2024-12-01 19:55:36 -0500

    fixed the wheel name

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,6 +8,6 @@ unset PYO3_CROSS_PYTHON_IMPLEMENTATION
 export LIBCLANG_PATH=$BUILD_PREFIX/lib
 
 maturin build --features python -vv -j 1 --release --strip --manylinux off --interpreter=${PYTHON}
-${PYTHON} -m pip install $SRC_DIR/target/wheels/catsmothing*.whl --no-index --no-deps -vv
+${PYTHON} -m pip install $SRC_DIR/target/wheels/*.whl --no-index --no-deps -vv
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
